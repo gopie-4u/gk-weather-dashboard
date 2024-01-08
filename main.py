b55eb30bed17a4d78a5002fbda3c4863f95450cc
GK <gopikrishna4u@gmail.com>
2024-01-08 19:09:41 +0530

    Added dates under images

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if place:
         if option == "Temperature":
             temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
             dates = [dict["dt_txt"] for dict in filtered_data]
-            print(dates)
+            # print(dates)
             # Create a temperature plot
             figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
             st.plotly_chart(figure)
@@ -28,8 +28,9 @@ if place:
             images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
                       "Rain": "images/rain.png", "Snow": "images/snow.png"}
             sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
             image_paths = [images[condition] for condition in sky_conditions]
-            print(sky_conditions)
-            st.image(image_paths, width=115)
+            # print(sky_conditions)
+            st.image(image_paths, caption=dates, width=115)
     except KeyError:
         st.write("That place does not exists ")
